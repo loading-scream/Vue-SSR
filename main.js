f7cb7b9fb4f6fcf7f3086753d2a61e4d7b1b5dcb
loading <dingbenye@shein.com>
2020-01-20 14:17:55 +0800

    feat: 封装app工厂函数的背景原因

--- main.js
+++ main.js
@@ -10,11 +10,8 @@ const server = require('express')()
 
 // 写一个正则路由
 server.get(/^\/index\/(p-(\d+))?$/, (req, res) => {
-    // 匹配中间件进来
-
-    // 创建App
+    console.log(req.url);
     const app = new Vue({
-        // 接入来自URL的参数
         data: {
             url: req.url,
             query: req.query,
@@ -26,29 +23,45 @@ server.get(/^\/index\/(p-(\d+))?$/, (req, res) => {
             <div>请求params参数: {{params}}</div>
         </div>`
     })
-
-    // 使用ssr工具把app渲染成html(这里是服务端代码) 
     // 模板也支持插值, 要单独写出来不能放app中, 类似模板专用的data
     const context = {
         title: req.query.title,
         meta: `<meta charset="UTF-8">`
     }
+    // 使用ssr工具把app渲染成html(这里是服务端代码) 
     renderer.renderToString(app, context, (err, html) => {
-
-        // 若出错直接响应code 500 服务端内部错误
         if (err) return res.status(500).end('Internal Server Error: ' + err)
-
-        // 直出html到浏览器并识别
         res.header('content-type', 'text/html') //中文处理
         res.end(html)
-
     })
+})
 
-    // 不传callback时, 返回Promise (不再演示)
-    // renderer.renderToString(app)
-    //     .then(html => console.log(html))
-    //     .catch(err => console.log(err))
-
+// 再写一个正则路由
+server.get(/^\/user\/(p-(\d+))?$/, (req, res) => {
+    console.log(req.url);
+    // 创建App (每个请求来自不同客户端, 必须重新创建app防止状态污染, 上同)
+    const app = new Vue({
+        data: {
+            url: req.url,
+            query: req.query,
+            params: req.params
+        },
+        template: `<div>
+            <div>访问URL: {{url}}</div>
+            <div>请求query参数: {{query}}</div>
+            <div>请求params参数: {{params}}</div>
+        </div>`
+    })
+    const context = {
+        title: req.query.title,
+        meta: `<meta charset="UTF-8">`
+    }
+    renderer.renderToString(app, context)
+        .then(html => {
+            res.header('content-type', 'text/html')
+            res.end(html)
+        })
+        .catch(err => err && res.status(500).end('Internal Server Error: ' + err))
 })
 
 server.listen(8080, () => {
