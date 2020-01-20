88125eb5de3846d992ce16a40ebeb0442201c3f1
loading <dingbenye@shein.com>
2020-01-20 11:50:17 +0800

    feat: 抽离H5模板

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // 导入Vue
 const Vue = require('vue')
 // 导入SSR工具
-const renderer = require('vue-server-renderer').createRenderer()
+const renderer = require('vue-server-renderer').createRenderer({
+    // H5页面基本架构抽离到模板, utf-8格式读取模板, ssr识别坑位自动拼接好html
+    template: require('fs').readFileSync('layout.html', 'utf-8')
+})
 // 导入 web 服务器框架
 const server = require('express')()
 
@@ -23,18 +26,12 @@ server.get('*', (req, res) => {
     renderer.renderToString(app, (err, html) => {
 
         // 若出错直接响应code 500 服务端内部错误
-        if (err) return res.status(500).end('Internal Server Error')
+        if (err) return res.status(500).end('Internal Server Error: ' + err)
 
-        // 现阶段拼好H5页面基本架构返回结果, 试验能否直出html到浏览器并识别
+        // 直出html到浏览器并识别
         res.header('content-type','text/html') //中文处理
-        res.end(`
-        <!DOCTYPE html>
-        <html lang="en">
-          <head><title>Hello</title></head>
-          <body>${html}</body>
-        </html>
-      `)
-      
+        res.end(html)
+
     })
 
     // 不传callback时, 返回Promise (不再演示)
@@ -46,5 +43,4 @@ server.get('*', (req, res) => {
 
 server.listen(8080, () => {
     console.log('listen in port 8080');
-
 })
